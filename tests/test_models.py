4159ea97265e9f73b7beca9fe1ce26f8358fb150
sophyolivernoc <sophy.oliver@noc.ac.uk>
2025-01-16 11:59:40 +0000

    use parametrize to test different inputs

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,73 +8,38 @@ from inflammation.models import daily_mean
 from inflammation.models import daily_max
 from inflammation.models import daily_min
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+@pytest.mark.parametrize(
+    "test, expect",
+    [
+        ( [[0, 0],[0, 0],[0, 0]], [0, 0] ),
+        ( [[1, 2],[3, 4],[5, 6]], [3, 4] ),
+    ])
+def test_daily_mean_with_different_inputs(test, expect):
+    """ test daily mean with different inputs"""
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expect))
 
 
-def test_daily_max_zeros():
-    """Test that max function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expect",
+    [
+        ( [[0, 0],[0, 0],[0, 0]], [0, 0] ),
+        ( [[1, 2],[3, 4],[5, 6]], [5, 6] ),
+    ])
+def test_daily_max_with_different_inputs(test, expect):
+    """ test daily max with different inputs"""
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expect))
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
+    
+@pytest.mark.parametrize(
+    "test, expect",
+    [
+        ( [[0, 0],[0, 0],[0, 0]], [0, 0] ),
+        ( [[1, 2],[3, 4],[5, 6]], [1, 2] ),
+    ])
+def test_daily_min_with_different_inputs(test, expect):
+    """ test daily min with different inputs"""
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expect))
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-def test_daily_max_integers():
-    """Test that max function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([5, 6])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-def test_daily_min_zeros():
-    """Test that max function works for an array of zeros."""
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
-
-def test_daily_min_integers():
-    """Test that min function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([1, 2])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
 
 def test_wrong_input():
     """test for typerror"""
